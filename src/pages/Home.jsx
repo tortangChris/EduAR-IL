357034c76975cae1c180e7fdaef50ad2fa8e1299
tortangChris <eliseriochrisashley@gmail.com>
2025-04-19 01:56:17 +0800

    Add top bar to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold">Home Page</h1>
+    <div className="pt-4 px-4 pb-20 min-h-screen bg-base-100">
+      <div className="w-full text-center text-gray py-4 px-6 rounded-xl shadow-md">
+        <h1 className="text-lg font-semibold">EduAR:</h1>
+        <p className="text-s font-semibold">
+          Interactive Learning of Data Structure & Algorithms
+        </p>
+      </div>
     </div>
   );
 };
