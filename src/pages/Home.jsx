75467329d8d3df988d922a0727c95f9b39f66d19
tortangChris <eliseriochrisashley@gmail.com>
2025-04-19 01:59:19 +0800

    remove paragraph on top bar homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,8 @@ import React from "react";
 const Home = () => {
   return (
     <div className="pt-4 px-4 pb-20 min-h-screen bg-base-100">
-      <div className="w-full text-center text-gray py-4 px-6 rounded-xl shadow-md">
-        <h1 className="text-lg font-semibold">EduAR:</h1>
-        <p className="text-s font-semibold">
-          Interactive Learning of Data Structure & Algorithms
-        </p>
+      <div className="w-full text-gray py-4 px-6 rounded-xl shadow-md">
+        <h1 className="text-lg font-semibold">EduAR</h1>
       </div>
     </div>
   );
