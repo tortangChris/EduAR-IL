7559df2b544145961f96b70a78bf0e83937fbe64
tortangChris <eliseriochrisashley@gmail.com>
2025-04-19 18:47:16 +0800

    add settings content

--- src/components/SettingsPersonalNav.jsx
+++ src/components/SettingsPersonalNav.jsx
@@ -3,18 +3,18 @@ import React from "react";
 
 const SettingsPersonalNav = () => {
   return (
-    <div className="sticky top-0 z-10">
-      <div className="bg-base-200 rounded-2xl shadow-lg h-28 px-6 flex justify-between items-center max-w-md mx-auto">
-        <div className="flex items-start gap-4">
-          <UserCircleIcon className="w-12 h-12 text-primary" />
-          <div className="mt-1">
-            <h2 className="text-lg font-semibold">Full Name</h2>
-            <p className="text-sm text-gray-500">user@gmail.com</p>
-          </div>
+    // <div className="sticky top-0 z-10">
+    <div className="bg-base-200 rounded-2xl shadow-lg h-28 px-6 flex justify-between items-center w-full">
+      <div className="flex items-start gap-4">
+        <UserCircleIcon className="w-12 h-12 text-primary" />
+        <div className="mt-1">
+          <h2 className="text-lg font-semibold">Full Name</h2>
+          <p className="text-sm text-gray-500">user@gmail.com</p>
         </div>
-        <ChevronRight className="w-6 h-6 text-gray-400" />
       </div>
+      <ChevronRight className="w-6 h-6 text-gray-400" />
     </div>
+    // </div>
   );
 };
 
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import SettingsPersonalNav from "../components/SettingsPersonalNav";
+import SettingsContent from "../components/SettingsContent";
 
 const Settings = () => {
   return (
     <div className="h-screen p-4 bg-base-100 space-y-4">
       <SettingsPersonalNav />
+      <SettingsContent />
     </div>
   );
 };
