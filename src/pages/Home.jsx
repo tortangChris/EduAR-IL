ff15b22f703835c76fc4140b85cadb190718085c
tortangChris <eliseriochrisashley@gmail.com>
2025-04-19 09:00:58 +0800

    remove topbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return (
-    <div className="pt-4 px-4 pb-20 min-h-screen bg-base-100">
-      <div className="w-full text-gray py-4 px-6 rounded-xl shadow-md">
-        <h1 className="text-lg font-semibold">EduAR</h1>
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
