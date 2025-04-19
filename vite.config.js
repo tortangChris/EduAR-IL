00ca2320b82b448c5d7c4489ef470686348a0d28
tortangChris <eliseriochrisashley@gmail.com>
2025-04-19 12:07:21 +0800

    fix client-side routing 3

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    fs: {
-      allow: ["."],
-    },
-  },
-  build: {
-    outDir: "dist",
-  },
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
   base: "/",
 });
