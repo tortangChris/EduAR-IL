2602f42f0863d1f0d78c643100ef39f1993c16f4
tortangChris <eliseriochrisashley@gmail.com>
2025-04-23 23:18:00 +0800

    revise

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   const progress = 0;
 
   return (
-    <div className="flex flex-col min-h-[calc(100vh-4rem)] max-h-[calc(100vh-4rem)] overflow-y-auto p-4 bg-base-100 space-y-4">
+    <div className="h-[calc(100vh-4rem)] overflow-y-auto p-4 bg-base-100 space-y-4">
       <HomeHeader />
       <ProgressCard progress={progress} />
       <RecentActivity />
