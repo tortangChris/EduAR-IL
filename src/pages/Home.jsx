ce27a9a20ceae52ee75ddbd83cbd025050acc249
tortangChris <eliseriochrisashley@gmail.com>
2025-08-18 02:16:09 +0800

    Test 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,23 @@
-import React from "react";
-import { Canvas } from "@react-three/fiber";
-import { ARCanvas, DefaultXRControllers } from "@react-three/xr";
+import React, { useRef } from "react";
+import { ARCanvas, useHitTest } from "@react-three/xr";
 import { Text } from "@react-three/drei";
+import { Vector3 } from "three";
 
 const CubeWithLabel = () => {
+  const hitPosition = useRef(new Vector3());
+
+  useHitTest((hitMatrix) => {
+    hitMatrix.decompose(hitPosition.current, new Quaternion(), new Vector3());
+  });
+
   return (
     <>
-      {/* Cube */}
-      <mesh position={[0, 0, -1]}>
-        {" "}
-        {/* Slightly in front of camera */}
+      <mesh position={hitPosition.current}>
         <boxGeometry args={[0.2, 0.2, 0.2]} />
         <meshStandardMaterial color="#4f46e5" />
       </mesh>
-
-      {/* Label */}
       <Text
-        position={[0, 0.15, -1]} // on top of cube
+        position={[0, 0.15, 0]}
         fontSize={0.05}
         color="white"
         anchorX="center"
@@ -32,13 +33,12 @@ const Home = () => {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <ARCanvas
-        sessionInit={{ requiredFeatures: ["hit-test"] }} // Enable AR hit-test
+        sessionInit={{ requiredFeatures: ["hit-test"] }}
         camera={{ position: [0, 0, 0] }}
       >
         <ambientLight intensity={0.5} />
         <directionalLight position={[0.5, 1, 0.5]} intensity={1} />
         <CubeWithLabel />
-        <DefaultXRControllers />
       </ARCanvas>
     </div>
   );
